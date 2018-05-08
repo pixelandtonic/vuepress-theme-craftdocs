9b6d5c03177a0b2b0ffbfee531fa39a767aa7173
brandonkelly <brandon@pixelandtonic.com>
2018-05-08 11:44:35 -0700

    `::: code` container support

---
+++ components/CodeToggle.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="code-toggle">
+        <ul class="code-language-switcher">
+            <li v-for="language in languages">
+                <a :class="{ active: language === selectedLanguage }" @click="setLanguage(language)">{{ $site.themeConfig.codeLanguages[language] }}</a>
+            </li>
+        </ul>
+        <slot :selectedLanguage="selectedLanguage"></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['languages'],
+
+        data() {
+            return {
+                selectedLanguage: this.languages[0]
+            }
+        },
+
+        beforeCreate() {
+            // let languages = [];
+            // this.$slots.default.forEach(n => {
+            //
+            // });
+            // debugger;
+        },
+
+        computed: {
+            isActive(language) {
+                return language === this.selectedLanguage
+            }
+        },
+
+        methods: {
+            setLanguage(language) {
+                console.log(language);
+                this.selectedLanguage = language
+            }
+        }
+    }
+</script>
